55d01298bc4cf95a4121313a84ac50486f217ccd
kbarta8 <30446303+kbarta8@users.noreply.github.com>
2019-10-23 16:38:27 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,26 +1,26 @@
 import socket                   # Import socket module
+from Crypto.Cipher import AES   #https://pypi.org/project/pycrypto/
 
-s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)             # Create a socket object
-#host = ""  #Ip address that the TCPServer is there
-host = socket.gethostname()
-port = 50000                     # Reserve a port for your service every new transfer wants a new port or you must wait.
+
+s = socket.socket()             # Create a socket object
+host = "10.138.21.141"
+port = 7778                 # Reserve a port for your service.
 
 s.connect((host, port))
-s.send("Hello server!")
+l="Hello server!"
+b1 = bytes(l)
+s.send(b1)
 
 with open('received_file', 'wb') as f:
-    print ('file opened')
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
+	print ('file opened')
+    #if True:
+	print('receiving data...')
+	obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+	data = obj2.decrypt(s.recv(1024))
+	print('data=%s', (data))
+	f.write(data)
 
 f.close()
 print('Successfully get the file')
 s.close()
 print('connection closed')
-
